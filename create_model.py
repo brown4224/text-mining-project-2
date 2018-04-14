fb1153635e1a14e781a1fbdc98d0e8b347c97353
Sean McGlincy <swmcglincy@hotmail.com>
2018-04-14 08:53:14 -0400

    hacking Jacobs code

--- create_model.py
+++ create_model.py
@@ -54,11 +54,14 @@ def cosine_similarity(x,y):
 def stemming(tokens):
     return (PorterStemmer().stem(token) for token in analyzer(tokens))
 
-def one_hot(labels):
-    l = len(labels)
-    Y= np.zeros((l, 4))
-    for i in range(l):
-        Y[i][labels[i] - 1] = 1
+def multi_label(postions,length):
+    postions = postions.as_matrix()
+    Y= np.zeros((length, 4))
+    for query, postion, doc in  postions:
+        Y[doc - 1][postion - 1] = query
+        # Y[doc - 1][postion - 1] = 1
+    from sklearn.preprocessing import MultiLabelBinarizer
+    Y = MultiLabelBinarizer().fit_transform(Y)
     return Y
 
 
@@ -67,10 +70,13 @@ def create_model(all_documents_file, relevance_file,query_file):
     '''Step 1. Creating  a dataframe with three fields query, title, and relevance(position)'''
     documents = pd.read_json(all_documents_file)[["id", "title", "body"]]
     query_file = pd.read_json(query_file)[["query number","query" ]]
+    relevance_with_values = documents
+
 
     relevance = pd.read_json(relevance_file)[["query_num", "position", "id"]]
-    relevance_with_values = relevance.merge(query_file,left_on ="query_num", right_on="query number")[ ["id","query", "position"]]\
-        .merge(documents,left_on ="id", right_on="id") [["query", "position", "title", "body"]]
+    Y = multi_label(relevance, len(documents))
+    # relevance_with_values = relevance.merge(query_file,left_on ="query_num", right_on="query number")[ ["id","query", "position"]]\
+    #     .merge(documents,left_on ="id", right_on="id") [["query", "position", "title", "body"]]
 
 
     if(is_debug):
@@ -81,7 +87,7 @@ def create_model(all_documents_file, relevance_file,query_file):
 
     '''Step 2. Creating  a column for creating index'''
 
-    relevance_with_values ["all_text"] = relevance_with_values.apply( lambda x : x["query"] + x["title"] + x["body"] , axis =1)
+    relevance_with_values ["all_text"] = relevance_with_values.apply( lambda x : x["title"] + x["body"] , axis =1)
 
     ''' Step 3. Creating a model for generating TF feature'''
 
@@ -92,24 +98,26 @@ def create_model(all_documents_file, relevance_file,query_file):
 
     ''' Step 4. Saving the model for TF features'''
     joblib.dump(vectorizer, 'resources/vectorizer.pkl')
-
-    ''' Step 5. Converting query and title to vectors and finding cosine similarity of the vectors'''
-    relevance_with_values["doc_vec_title"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["title"] ]), axis =1)
-    relevance_with_values["doc_vec_body"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["body"]]), axis =1)
-    relevance_with_values["query_vec"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["query"]]), axis =1)
-    relevance_with_values["cosine_title"]  = relevance_with_values.apply(lambda x: cosine_similarity(x['doc_vec_title'], x['query_vec']), axis=1)
-    relevance_with_values["cosine_body"]  = relevance_with_values.apply(lambda x: cosine_similarity(x['doc_vec_body'], x['query_vec']), axis=1)
-
-
+    #
+    # ''' Step 5. Converting query and title to vectors and finding cosine similarity of the vectors'''
+    # relevance_with_values["doc_vec_title"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["title"] ]), axis =1)
+    # relevance_with_values["doc_vec_body"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["body"]]), axis =1)
+    # relevance_with_values["query_vec"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["query"]]), axis =1)
+    # relevance_with_values["cosine_title"]  = relevance_with_values.apply(lambda x: cosine_similarity(x['doc_vec_title'], x['query_vec']), axis=1)
+    # relevance_with_values["cosine_body"]  = relevance_with_values.apply(lambda x: cosine_similarity(x['doc_vec_body'], x['query_vec']), axis=1)
+    #
+    #
 
     ''' Step 6. Defining the feature and label  for classification'''
 
 
-    X = relevance_with_values[["cosine_title"] + ["cosine_body"] ]
-
+    # X = relevance_with_values[["cosine_title"] + ["cosine_body"] ]
+    # relevance_with_values["test"] = vectorizer.transform(relevance_with_values["title"])
+    X = vectorizer.transform(relevance_with_values["body"])
     # t = vectorizer.transform(relevance_with_values["title"])
-    Y = relevance_with_values["position"]
-
+    # Y = relevance_with_values["position"]
+    from sklearn import preprocessing
+    X = preprocessing.normalize(X, norm='l2')
 
     # # print ("Features: " , X.shape)
     # Y = [v for k, v in relevance_with_values["position"].items()]
@@ -127,7 +135,10 @@ def create_model(all_documents_file, relevance_file,query_file):
     X_train, X_test, y_train, y_test = train_test_split(    X, Y, test_size = 0.33, random_state = 42)
 
     ''' Step 8. Classification and validation'''
-    target_names = ['1', '2', '3','4']
+    target_names = ['1', '2', '3','4' ]
+    # target_names = []
+    # for i in range(1, 224):
+    #     target_names.append(str(i))
     clf = RandomForestClassifier().fit(X_train, y_train)
     print(classification_report(y_test,  clf.predict(X_test), target_names=target_names))
 
