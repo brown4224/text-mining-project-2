aa1d186135f11cf91abf317ac64ca89ec65c6061
Sean McGlincy <swmcglincy@hotmail.com>
2018-04-19 21:22:46 -0400

    starting idf features

--- create_model.py
+++ create_model.py
@@ -30,19 +30,30 @@ def load_models():
 '''
 
 def evaluate_model(model, query, document):
-    print (model)
-    query_vec = model[0].transform([query['query']])
-    title_vec = model[0].transform([document['title']])
-    body_vec = model[0].transform([document['body']])
+    query = query['query']
+    title = document['title']
+    body = document['body']
+    query_vec = model[0].transform([query])
+    title_vec = model[0].transform([title])
+    body_vec = model[0].transform([body])
+
+    common_title = common_terms(title, query)
+    common_body = common_terms(body, query)
+
+
 
     cos_title = cosine_similarity(query_vec, title_vec)
     cos_body = cosine_similarity(query_vec, body_vec)
 
-    result = model[1].predict([[cos_title, cos_body]])
+    result = model[1].predict([[cos_title, common_title, cos_body, common_body]])
     return result[0],0.5
 
 def common_terms(x,y):
-    x.split(" ").intersect(y.split(" "))
+    count = 0
+    for token in set(x.split(" ")):
+        if token in set(y.split(" ")):
+            count += 1
+    return count
 
 def cosine_similarity(x,y):
     cos = cosine(x.toarray()[0], y.toarray()[0])
@@ -62,6 +73,12 @@ def one_hot(labels):
     return Y
 
 
+
+def idf_metadata(X, L):
+    sum_idf = np.sum(X)
+    return float(sum_idf) / (len(L) + 1.0)
+
+
 def create_model(all_documents_file, relevance_file,query_file):
 
     '''Step 1. Creating  a dataframe with three fields query, title, and relevance(position)'''
@@ -73,19 +90,13 @@ def create_model(all_documents_file, relevance_file,query_file):
         .merge(documents,left_on ="id", right_on="id") [["query", "position", "title", "body"]]
 
 
-    if(is_debug):
-        print ("Doc: " , documents.shape)
-        print ("query: " , query_file.shape)
-        print ("relavence: " , relevance.shape)
-        print ("relavence _ values : " , relevance_with_values.shape)
-
     '''Step 2. Creating  a column for creating index'''
 
     relevance_with_values ["all_text"] = relevance_with_values.apply( lambda x : x["query"] + x["title"] + x["body"] , axis =1)
 
     ''' Step 3. Creating a model for generating TF feature'''
 
-    # vectorizer = TfidfVectorizer( stop_words="english", lowercase=True, norm="l2", analyzer=stemming)
+    # vectorizer = TfidfVectorizer(min_df=0.0, max_df=1.0, stop_words="english", lowercase=True, norm="l2", strip_accents='ascii')
     vectorizer = TfidfVectorizer(min_df=0.0, max_df=1.0, stop_words="english", lowercase=True, norm="l2", strip_accents='ascii')
     vectorizer = vectorizer.fit(relevance_with_values["all_text"])
 
@@ -94,33 +105,25 @@ def create_model(all_documents_file, relevance_file,query_file):
     joblib.dump(vectorizer, 'resources/vectorizer.pkl')
 
     ''' Step 5. Converting query and title to vectors and finding cosine similarity of the vectors'''
+    relevance_with_values["query_vec"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["query"]]), axis =1)
     relevance_with_values["doc_vec_title"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["title"] ]), axis =1)
     relevance_with_values["doc_vec_body"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["body"]]), axis =1)
-    relevance_with_values["query_vec"] = relevance_with_values.apply(lambda x: vectorizer.transform([x["query"]]), axis =1)
     relevance_with_values["cosine_title"]  = relevance_with_values.apply(lambda x: cosine_similarity(x['doc_vec_title'], x['query_vec']), axis=1)
     relevance_with_values["cosine_body"]  = relevance_with_values.apply(lambda x: cosine_similarity(x['doc_vec_body'], x['query_vec']), axis=1)
+    relevance_with_values["common_title"] = relevance_with_values.apply(lambda x: common_terms(x["title"], x["query"] ), axis =1)
+    relevance_with_values["common_body"] = relevance_with_values.apply(lambda x: common_terms(x["body"], x["query"] ), axis =1)
 
 
+    relevance_with_values["max_title_idf"] = relevance_with_values.apply(lambda x: np.max(x["doc_vec_title"]), axis =1)
+    relevance_with_values["sum_title_idf"] = relevance_with_values.apply(lambda x: idf_metadata(x["doc_vec_title"], x["title"] ), axis =1)
+    relevance_with_values["max_body_idf"] = relevance_with_values.apply(lambda x: np.max(x["doc_vec_body"] ), axis =1)
+    relevance_with_values["sum_body_idf"] = relevance_with_values.apply(lambda x: idf_metadata(x["doc_vec_body"], x["body"] ), axis =1)
 
     ''' Step 6. Defining the feature and label  for classification'''
-
-
-    X = relevance_with_values[["cosine_title"] + ["cosine_body"] ]
-
-    # t = vectorizer.transform(relevance_with_values["title"])
-    Y = relevance_with_values["position"]
-
-
-    # # print ("Features: " , X.shape)
-    # Y = [v for k, v in relevance_with_values["position"].items()]
-    # Y = one_hot(Y)
-
-
-    if(is_debug):
-        print ("Lables: ", Y)
-        print ("Data shape: ", X.shape)
-        print ("Label shape: ", len(Y))
-
+    X = relevance_with_values[  ["cosine_title"]+ ["common_title"] + ["max_title_idf"] + ["sum_title_idf"] + ["cosine_body"] + ["common_body"]  + ["max_body_idf"] + ["sum_body_idf"]]
+    # X = relevance_with_values[ ["max_query_idf"] + ["sum_query_idf"] + ["cosine_title"]+ ["common_title"] + ["max_title_idf"] + ["sum_title_idf"] + ["cosine_body"] + ["common_body"]   ]
+    Y = [v for k, v in relevance_with_values["position"].items()]
+    # Y = relevance_with_values["position"]
 
 
     ''' Step 7. Splitting the data for validation'''
@@ -128,7 +131,9 @@ def create_model(all_documents_file, relevance_file,query_file):
 
     ''' Step 8. Classification and validation'''
     target_names = ['1', '2', '3','4']
+    # clf = MultinomialNB().fit(X_train, y_train)
     clf = RandomForestClassifier().fit(X_train, y_train)
+
     print(classification_report(y_test,  clf.predict(X_test), target_names=target_names))
 
     ''' Step 9. Saving the data '''
--- eval.py
+++ eval.py
@@ -64,6 +64,6 @@ if __name__ == '__main__':
             dcg = calculate_dcg(query, documents)
             idcg = ideal_ndcg[str(query['query number'])]
             ndcg = dcg / idcg
-            print ("dcg={}, ideal={}, ndcg={}".format(dcg, idcg, ndcg))
+            print("dcg={}, ideal={}, ndcg={}".format(dcg, idcg, ndcg))
             sum = sum + ndcg
-    print ("Final ncdg for all queries is {}".format(sum / len(all_queries)))
+    print("Final ncdg for all queries is {}".format(sum / len(all_queries)))
